f69436243124df8e3b8811f7836e8775f1e6c6b2
Devix Rootix <shukrik27@gmail.com>
2019-11-23 09:27:21 +0300

    No ideas what it is it

---
+++ build/precache-manifest.1dcf3245758c92825663c05929151b79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c24afc94281571d762a6e24c46d5fd",
+    "url": "/todoix/index.html"
+  },
+  {
+    "revision": "b6867ff4adeab667b94f",
+    "url": "/todoix/static/css/main.6d651572.chunk.css"
+  },
+  {
+    "revision": "023cb44d64fd85a10447",
+    "url": "/todoix/static/js/2.2e817f00.chunk.js"
+  },
+  {
+    "revision": "b6867ff4adeab667b94f",
+    "url": "/todoix/static/js/main.5bc2cdec.chunk.js"
+  },
+  {
+    "revision": "949447bd61460333cd10",
+    "url": "/todoix/static/js/runtime-main.fc4cbc28.js"
+  }
+]);

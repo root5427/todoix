04d6582e4d24776df3358a528d37a7c95dbd676c
Devix Rootix <shukrik27@gmail.com>
2019-11-23 09:17:00 +0300

    Updates

---
+++ precache-manifest.b058c65341ddee3d35442a4039577529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd833c8030a0204cb62c5e313c3716b",
+    "url": "/todoix/index.html"
+  },
+  {
+    "revision": "73d7b491dbe7b45ed133",
+    "url": "/todoix/static/css/main.6d651572.chunk.css"
+  },
+  {
+    "revision": "023cb44d64fd85a10447",
+    "url": "/todoix/static/js/2.2e817f00.chunk.js"
+  },
+  {
+    "revision": "73d7b491dbe7b45ed133",
+    "url": "/todoix/static/js/main.cf42741e.chunk.js"
+  },
+  {
+    "revision": "949447bd61460333cd10",
+    "url": "/todoix/static/js/runtime-main.fc4cbc28.js"
+  }
+]);
